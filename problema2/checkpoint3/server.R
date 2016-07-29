f47d03874b598e9a8737128265d46646eafe173b
Rafaelle Amorim Monteiro Xavier 21211955 <rafaelle.xavier@gmail.com>
2016-07-29 17:05:45 -0300

    Shiny

---
+++ problema2/checkpoint3/server.R
@@ -0,0 +1,46 @@
+#
+# This is the server logic of a Shiny web application. You can run the 
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(dplyr, warn.conflicts = F)
+library(readr)
+library(ggplot2)
+theme_set(theme_bw())
+
+gastos = read_csv("~/AD1/dados/ano-atual.csv")
+
+emissao.bilhete.aereo = gastos %>%
+  group_by(txtDescricao) %>%
+  filter(txtDescricao == "Emissão Bilhete Aéreo" & vlrLiquido >= 0)
+
+
+bilhete.aereo.por.deputado = emissao.bilhete.aereo %>%
+  group_by(sgUF, txNomeParlamentar) %>%
+  summarise(total.passageiros = length(unique(txtPassageiro)), 
+            total.vlrLiquido = sum(vlrLiquido), 
+            gasto.medio.bilhete = mean(total.vlrLiquido/length(unique(txtNumero))), 
+            quantidade.bilhetes = length(unique(txtNumero)))  %>%
+  arrange(-total.passageiros)
+
+shinyServer(function(input, output) {
+  
+  
+  output$plotEstado = renderPlot({
+    bilhete.aereo.por.deputado %>% 
+      filter(sgUF == input$checkbox) %>% 
+      ggplot(aes(quantidade.bilhetes, gasto.medio.bilhete, fill = sgUF)) + 
+      geom_point(position = position_jitter(width = .5), alpha = .4, size = bilhete.aereo.por.deputado$total.passageiros)
+  })
+   
+  
+  output$view <- renderTable({
+    bilhete.aereo.por.deputado[bilhete.aereo.por.deputado$sgUF == input$checkbox, ]
+  })
+  
+})
---
+++ problema2/checkpoint3/ui.R
@@ -0,0 +1,31 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
+# Define UI for application that draws a histogram
+shinyUI(fluidPage(
+  
+  # Application title
+  headerPanel("Emissão de Bilhete Aéreo"),
+  
+  sidebarPanel(
+    checkboxGroupInput("checkbox", 
+                       label = "Estados:", 
+                       choices = levels(as.factor(gastos$sgUF))),
+    
+    mainPanel(
+      plotOutput(outputId = "plotEstado")
+    )
+    
+    
+    
+  )
+  
+))
